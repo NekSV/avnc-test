2145fbdc98f1bf5891a24c8a4ae277e64c0c2ef2
Alan <alankvm@gmail.com>
2022-11-03 22:24:49 -0600

    Employees and Genre with relationships with turns and employees

--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -5,19 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Branches extends Model
+class Employees extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'turns_id'];
+    protected $fillable = ['name', 'employee_code', 'first_name', 'last_name',
+        'birthdate', 'email', 'country_id', 'city', 'address', 'phone', 'hiring_date',
+        'status', 'genres_id'];
 
     public function turns(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Turns::class);
-    }
-
-    public function employess(): \Illuminate\Database\Eloquent\Relations\HasMany
-    {
-        return $this->hasMany(Employees::class);
     }
 }
